72977c3af0f4664fc06836a15ecde2b6b5897295
Aathif1118 <aathifahamed66@gmail.com>
2026-05-22 00:02:44 +0530

    created the RegularUser class

---
+++ src/main/java/com/bikeride/Model/RegularUser.java
@@ -0,0 +1,32 @@
+package com.bikeride.model;
+
+// INHERITANCE: RegularUser EXTENDS User (gets all of User's methods)
+public class RegularUser extends com.bikeride.model.User {
+
+    private int totalRides;
+
+    public RegularUser(String userId, String username,
+                       String email, String password) {
+        // Call parent constructor using super()
+        super(userId, username, email, password, "REGULAR");
+        this.totalRides = 0;
+    }
+
+    // Add this getter for JSP compatibility
+    public String getName() {
+        return this.getUsername(); // or just return username if you have it
+    }
+
+    public int getTotalRides() { return totalRides; }
+    public void setTotalRides(int totalRides) { this.totalRides = totalRides; }
+
+    // POLYMORPHISM: implementing the abstract method from User
+    @Override
+    public String getRole() { return "Regular Member"; }
+
+    // POLYMORPHISM: overriding parent method with different output
+    @Override
+    public String getDisplayInfo() {
+        return "Member: " + getUsername() + " | Rides: " + totalRides;
+    }
+}
