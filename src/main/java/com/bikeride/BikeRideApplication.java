66992fbef35616fb52fdbca63254ec13e5f853c4
Aathif1118 <aathifahamed66@gmail.com>
2026-05-21 23:56:22 +0530

    initialised the project

---
+++ src/main/java/com/bikeride/BikeRideApplication.java
@@ -0,0 +1,13 @@
+package com.bikeride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikeride/BikeRidePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikeride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRidePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
